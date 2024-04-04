e2fa6ea3e38086f564b5064c3a6bbce76c4424b5
Saugat Dawadi <saugatdwd@gmail.com>
2024-04-04 19:12:42 +0545

    Logo is added

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,6 +1,7 @@
 import  { useState } from "react";
 import { easeInCubic, FlipProvider, useFlip } from "react-easy-flip";
 import "../../style.css";
+import logo from '../../assets/images/logo.png'
 
 const tabs = [
   {
@@ -39,10 +40,18 @@ export default function Header() {
 
   return (
     <FlipProvider>
-      <div className="main w-full flex justify-center pt-3 border shadow-xl" data-flip-root-id={flipRootId}>
+      <div className="main w-full flex lg:gap-96  md:gap-28 sm:gap-20 pt-3 border shadow-xl" data-flip-root-id={flipRootId}>
+
+        <div className="w-40 h-20 flex justify-start">
+      <img src={logo} alt="" />
+        </div>
         <div className="flex w-1/2 justify-center items-center">
           {tabs?.map((item, index) => {
             return (
+              <>
+
+              <div>
+              </div>
               <div
                 onClick={() => selectedTabHandler(item.id)}
                 className="flex-col w-full text-center cursor-pointer hover:text-red-800"
@@ -56,6 +65,7 @@ export default function Header() {
                   <div className="non-active-tab" />
                 )}
               </div>
+              </>
             );
           })}
         </div>
