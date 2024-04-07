209ae7cbf10a10b36e5b5df1c95543c0bff130d9
Saugat Dawadi <saugatdwd@gmail.com>
2024-04-07 17:57:48 +0545

    For datepicker

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,10 +3,10 @@ export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
+    "./node_modules/tailwind-datepicker-react/dist/**/*.js", // <--- Add this line
   ],
   theme: {
     extend: {},
   },
   plugins: [],
 }
-
